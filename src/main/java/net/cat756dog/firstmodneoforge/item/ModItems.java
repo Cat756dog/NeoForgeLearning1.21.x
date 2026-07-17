fa249ed3c72eabb46264b9a932304de61bea858e
Cat756dog <nikolaostsirimokos@gmail.com>
2026-07-17 15:09:54 -0400

    Added and changed some stuff

---
+++ src/main/java/net/cat756dog/firstmodneoforge/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.cat756dog.firstmodneoforge.item;
+
+import net.cat756dog.firstmodneoforge.FirstMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static  final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FirstMod.MOD_ID);
+
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
